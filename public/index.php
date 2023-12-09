84aa30f2807b0f0005fa2ce5c136cb287fb68ea7
whatisilence <vazzoritips@gmail.com>
2023-12-09 12:23:18 +0500

    lesson 3 add namespace

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+//error_reporting(-1);
+
+use vendor\core\Router;
 
 $query = rtrim( $_SERVER['QUERY_STRING'], '/');
 
@@ -7,11 +10,10 @@ define('CORE', dirname(__DIR__) . '/vendor/core');
 define('ROOT', dirname(__DIR__));
 define('APP', dirname(__DIR__) . '/app');
 
-require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
 spl_autoload_register(function ($class){
-    $file = APP . "/controllers/$class.php";
+    $file = ROOT . '/' . str_replace('\\', '/', $class). '.php';
     if(is_file($file)){
         require_once $file;
     }
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace vendor\core;
+
 class Router
 {
 
@@ -40,7 +42,7 @@ class Router
 
     public static function dispatch($url){
         if(self::matchRoute($url)){
-            $controller = self::upperCamelCase(self::$route['controller']);
+            $controller = 'app\controllers\\' . self::upperCamelCase(self::$route['controller']);
             if(class_exists($controller)){
                 $cObj = new $controller;
                 $action = self::lowerCamelCase(self::$route['action'].'Action');
