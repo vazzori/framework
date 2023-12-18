d2cfc3347401c0bd0b41b51162e44f9dce3affe7
whatisilence <vazzoritips@gmail.com>
2023-12-18 22:44:36 +0500

    orm

---
+++ public/test.php
@@ -0,0 +1,30 @@
+<?php
+require 'rb.php';
+$db = require '../config/config_db.php';
+
+R::setup($db['dsn'], $db['user'], $db['pass']);
+R::freeze(true);
+R::fancyDebug(true);
+//var_dump(R::testConnection());
+
+//create
+//$category = R::dispense('category');
+//$category->title = 'Категория 2';
+//$id = R::store($category);
+//var_dump($id);
+
+//read
+//$category = R::load('category', 8);
+//echo $category['title']; // $category->title
+
+//update
+//$category = R::load('category', 8);
+//echo $category->title . '<br>';
+//$category->title = 'NEWNEWNEW';
+//R::store($category);
+//echo $category->title . '<br>';
+
+//delete
+//$category = R::load('category', 8);
+//R::trash($category);
+//R::wipe('category');
